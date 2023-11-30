149fa60effe364590ed40d51e84530c329aa8a70
Filipe Abner <filipeabner18@gmail.com>
2023-11-30 09:17:29 -0300

    Adiciona protótipo de fprintf

--- trab.s
+++ trab.s
@@ -3,13 +3,13 @@
 
 .equ SYS_read, 0 		# read
 .equ SYS_write, 1 		# write
-.equ SYS_open,2 		# file open
-.equ SYS_close,3 		# file close
-.equ SYS_creat,85 		# file open/create
+.equ SYS_open, 2 		# file open
+.equ SYS_close, 3 		# file close
+.equ SYS_creat, 85 		# file open/create
 
-.equ O_RDONLY,0			# read only
-.equ O_WDONLY,1			# write only
-.equ O_RDWR,2			# read and write
+.equ O_RDONLY, 0			# read only
+.equ O_WDONLY, 1			# write only
+.equ O_RDWR, 2			# read and write
 
 .equ O_CREAT, 0100		
 .equ O_TRUNC, 01000
@@ -25,9 +25,17 @@
 .equ STRLEN_LINE, 11
 .equ STRLEN_OUPUT, 17
 
+.equ END_OF_STRING, 0
+.equ PERCENT, 37
+.equ LETTER_S, 115
+.equ LETTER_L, 108
+.equ LETTER_D, 100
+.equ LETTER_C, 99
+
 .section .data
 	enterMsgString: .string "Enter a String: "
 	input_file: 	.string "input.txt"
+	teste: .string "tesfl"
 
 .section .bss
 .lcomm line, STRLEN_LINE
@@ -70,7 +78,6 @@ movq 	$line, %rsi			# string address
 movq 	$STRLEN_LINE, %rdx	# length value
 syscall
 
-
 popq 	%rbp
 ret
 
@@ -84,15 +91,92 @@ syscall
 popq %rbp
 ret
 
+# %rdi : file descriptor
+# %rsi : endereço da string na pilha
+# ... : 0..* variáveis na pilha
+fprintf:
+	pushq %rbp
+	movq %rsp, %rbp
+
+	# %rax guarda posição atual da string
+	movq $0, %rax
+
+	# numero de porcentagens na string
+	movq $0, %rcx
+
+	
+	# enquanto nao for fim do vetor
+	condicao_1_fprintf:
+		movb (%rsi, %rax, 1), %bl
+		cmpb $END_OF_STRING, %bl # Checa se terminou a string
+		je fim_while_1_fprintf
+
+		#	verifica posicaoatual se é porcentagem
+		# 	se for porcentagem
+		cmpb $PERCENT, %bl
+		jne fim_if_1
+			# incrementa posicao atual
+			incq %rax
+
+			# verifica qual o caractere após a porcentagem
+			movb (%rsi, %rax, 1), %bl
+
+			# salta para rotulo especifico para tratar esse caractere
+			# %s
+			cmpb $LETTER_S, %bl
+			jne rotulo_ld
+			# call 
+
+			# %ld
+			rotulo_ld:
+			cmpb $LETTER_L, %bl
+			incq %rax # vai para o 'd' do 'ld' (possivelmente há tratamento de erro para %l)
+			jne rotulo_c
+			# call
+
+			# %c
+			rotulo_c:
+			cmpb $LETTER_C, %bl
+			jne rotulo_fim_switch
+			# call
+			
+			rotulo_fim_switch:
+
+			incq %rcx # Quantos caracteres (temporário)
+
+		fim_if_1:
+
+	
+
+	
+	
+	# 	se nao for porcentagem
+	# 		imprime o caractere na saida
+	# 	incrementa posicao atual
+		incq %rax
+	
+		# jne inicio_while_1_fprintf
+	# termina
+		jmp condicao_1_fprintf
+	fim_while_1_fprintf:
+	# movsbq %bl, %rax
+	popq %rbp
+	ret
+
 _start:
 pushq %rbp
+movq %rsp, %rbp
 
-call fopen_SB
-call fclose_SB
+# call fopen_SB
+# call fclose_SB
+
+movq $teste, %rsi
+
+call fprintf
+movq %rax, %rdi
 
 popq %rbp
 
 movq 	$SYS_exit, %rax
-movq %rax, %rdi
 movq $60, %rax
 syscall
